6346d2625d2c510aa17265d768e8cee0049802c6
berenice2003 <67970560+berenice2003@users.noreply.github.com>
2020-08-10 11:31:44 -0500

    Add files via upload

---
+++ Berenice_C_Function Excersice.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+
+var str = "Hello, Everybody"
+
+func greet(person:String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
+    }
+
+print(greet(person: "Berenice"))
+
+    var a = 10
+    var b = 40
+
+func plus(min: Int, max: Int) -> Int {
+    let a = 10
+    let b = 40
+    return a + b
+}
+print(plus(min: a, max: b))
